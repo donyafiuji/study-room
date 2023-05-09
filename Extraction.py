6df225cc36ec115b14bc095ab2fc8d5fa1164bc1
donyafiuji <donya365fi.df@gmail.com>
2023-05-09 15:19:11 +0430

    modified:   .gitignore 	modified:   Extraction.py 	modified:   code.ipynb

--- Extraction.py
+++ Extraction.py
@@ -41,21 +41,25 @@ class Model():
         return matches, dialogues,start_times, end_times
 
 
-    def tag(self, path):
+    def tag(self, path, season_dir):
         
         matches = self.DialogueExtraction(path)[0]
         for match in matches:
             start_time, end_time, text = match
+            time = f'start time = {start_time} , end time = {end_time}'
+
             if text.startswith ('The One W'):
-                tag = f'season 1 , {text}'
-                time = f'start time = {start_time} , end time = {end_time}'
+                tag = f'season {season_dir}, {text}'
+            else:
+                tag = ''
 
         return tag, time
 
 
     def StopWords(self):
 
-        my_stopwords = set(['the','a', 'an', 'and', 'but', 'or', '1','0','2','3','4','5','6','7','8','liza','brothers','sisters','im','theres','ill','thats','goes','isnt','thats','took','youre','ive','hed','wasnt','la','wee',
+        my_stopwords = set(['the','id','guys',
+                            'a', 'an', 'and', 'but', 'or', '1','0','2','3','4','5','6','7','8','liza','brothers','sisters','im','theres','ill','thats','goes','isnt','thats','took','youre','ive','hed','wasnt','la','wee',
     '9','00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 
     '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', 
     '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', 
